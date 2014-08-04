692b4cf306d3dc1d89c6e3a80d343b4cb296b84a
Zydrunas Gimbutas <zydrunas.gimbutas@gmail.com>
2014-08-04 15:06:28 -0600

    Initial revision

---
+++ Contents.m
@@ -0,0 +1,16 @@
+%
+% Nonuniform FFT in R^3.
+%   nufft3d1 - Type 1 - Evaluate linearly spaced Fourier modes (Type 1 nuFFT).
+%   nufft3d2 - Type 2 - Evaluate function values (inverse of Type 1 nuFFT).
+%   nufft3d3 - Type 3 - Evaluate arbitrary located Fourier modes (Type 3 nuFFT).
+%
+% Direct (slow) evaluation of nonuniform FFT in R^3.
+%   dirft3d1 - Type 1 - Evaluate linearly spaced Fourier modes (Type 1 nuFFT).
+%   dirft3d2 - Type 2 - Evaluate function values (inverse of Type 1 nuFFT).
+%   dirft3d3 - Type 3 - Evaluate arbitrary located Fourier modes (Type 3 nuFFT).
+%
+% Test for nuFFT in R^3.
+%   test_3d1 - Test Type 1 nuFFT.
+%   test_3d2 - Test Type 2 nuFFT.
+%   test_3d3 - Test Type 3 nuFFT.
+%
